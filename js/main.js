4308049e3c7622e9ca2a64e44997736d0b83d5bc
Ganesh S Acharya <33441668+khazaddoom@users.noreply.github.com>
2019-09-16 22:08:27 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -12,7 +12,7 @@ $("#view-work").on("click", function() {
 if('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
     navigator.serviceWorker
-    .register('../sw_cached_pages.js')
+    .register('./sw_cached_pages.js')
     .then(reg => console.log("Registered"))
     .catch(err => console.log(err))
   })

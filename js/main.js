5be85c9d51c04f7fed747ffba5a13dcd8f503e2a
khazadoom <31121986g@gmail.com>
2019-09-19 08:12:36 +0530

    Intersection Observer learning

--- js/main.js
+++ js/main.js
@@ -17,3 +17,25 @@ if('serviceWorker' in navigator) {
     .catch(err => console.log(err))
   })
 }
+
+
+const targets = document.querySelectorAll('img');
+
+const lazyLoad = target => {
+  const io = new IntersectionObserver((entries, observer) => {
+    entries.forEach(entry => {
+      if(entry.isIntersecting) {
+        
+        const img = entry.target;
+        const src = img.getAttribute('data-lazy');
+        img.src = src;
+        
+        observer.disconnect();
+
+      }
+    });
+  });
+  io.observe(target);
+}
+
+targets.forEach(lazyLoad);
